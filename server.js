62305fb62a1566880ff2701b75652333aceb2182
Shannon <shannonjburris@gmail.com>
2021-08-17 21:06:54 -0400

    some routes  running

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// will change on deployment
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
